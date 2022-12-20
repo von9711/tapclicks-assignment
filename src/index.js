4aa1b22050de8d7a285a6907bc4d94dede9db474
von9711 <62811612+von9711@users.noreply.github.com>
2022-12-20 11:31:58 +0530

    add index.js

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const { sequelize } = require("./configs/db");
+const csv = require("csv-parser");
+const fs = require("fs/promises");
+const path = require("path");
+const {
+  Campaign,
+  CampaignLog,
+  Order,
+  OrderLog,
+  Creative,
+  CreativeLog,
+} = require("./models");
+const { addFileDataToDB } = require("./utility/db");
+const { getDateString } = require("./utility/util");
+
+const processAllFiles = async () => {
+  try {
+    const dirPath = path.resolve("src/data_files");
+    const files = await fs.readdir(dirPath);
+
+    for (let file of files) {
+      const fileDate = new Date(getDateString(file));
+      if (fileDate.getMonth() === 4) {
+        const fileUrl = path.join(dirPath, file);
+        await addFileDataToDB(fileUrl);
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+sequelize.sync({ alter: true }).then(() => {
+  console.log("sync done");
+  //   processAllFiles();
+  addFileDataToDB(
+    "/home/avilio/program/tapclicks-assignment/src/data_files/Yashi_2016-05-30.csv"
+  );
+});
